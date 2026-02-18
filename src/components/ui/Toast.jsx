979ffe047183534e30efe17cb062f1d3be2a42d4
FabioCoutinho <fabiofarias549@gmail.com>
2026-02-18 11:31:20 -0300

    improving the appearance of toast file

--- src/components/ui/Toast.jsx
+++ src/components/ui/Toast.jsx
@@ -1,17 +1,14 @@
 const Toast = ({ text, type = "success" }) => {
   const styleMsg = {
-    success: "bg-green-500",
-    error: "bg-red-500",
+    success: "bg-green-200 text-green-700",
+    error: "bg-red-200 text-red-700",
   };
 
   return (
-    <div className="fixed z-50 bottom-15 left-2 bg-gray-800  text-gray-400 font-bold p-3 rounded-lg">
-      <p className="flex gap-3 items-center">
-        <span
-          className={`inline-block w-4 h-4 ${styleMsg[type]} rounded-full`}
-        ></span>{" "}
-        {text}
-      </p>
+    <div
+      className={`fixed z-50 top-3 right-3 ${styleMsg[type]} font-bold p-3 rounded-lg`}
+    >
+      <p className="flex gap-3 items-center"> {text}</p>
     </div>
   );
 };
