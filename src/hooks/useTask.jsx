c76e4f3652c14c39c2f561ee3776e47c0e2374c8
FabioCoutinho <fabiofarias549@gmail.com>
2026-02-18 10:14:19 -0300

    moving the files to the correct position

---
+++ src/hooks/useTask.jsx
@@ -0,0 +1,58 @@
+import { taskServic } from "../services/taskServices";
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+import { useToast } from "./useToast";
+
+export const useTask = () => {
+  const { tasks, setTasks, filter } = useContext(TaskContext);
+  const { showMsg } = useToast();
+
+  const loadTask = async () => {
+    try {
+      const data = await taskServic.getTask();
+      setTasks(data);
+    } catch (err) {
+      alert("Não foi Possivel carregar as tarefas");
+    }
+  };
+
+  const createNewTask = async (name) => {
+    try {
+      const newTask = await taskServic.createTask(name);
+      setTasks((prevTask) => [...prevTask, newTask]);
+      showMsg("Tarefa adicionada com sucesso", "success");
+    } catch (err) {
+      showMsg("Erro ao criar nova tarefa", "error");
+    }
+  };
+
+  const upDataTask = async (field, value, id) => {
+    try {
+      const taskUpDate = await taskServic.upDateTaskField(field, value, id);
+      setTasks((prevTask) =>
+        prevTask.map((el) => (el.id === id ? taskUpDate : el)),
+      );
+      showMsg("Tarefa atualizada", "success");
+    } catch (err) {
+      showMsg("Erro ao atualizar a tarefa", "error");
+    }
+  };
+
+  const deleteTask = async (id) => {
+    try {
+      const newTask = await taskServic.Delete(id);
+      setTasks((prevTask) => prevTask.filter((el) => el.id !== id));
+      showMsg("Tarefa Apagada", "success");
+    } catch (error) {
+      showMsg("Erro ao apagar a tarefa", "error");
+    }
+  };
+  return {
+    tasks,
+    filter,
+    createNewTask,
+    loadTask,
+    upDataTask,
+    deleteTask,
+  };
+};
